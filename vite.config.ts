6164f6057da9927849d16ba103141d694da7786e
stlimhuno <stlim@huno.kr>
2025-12-15 16:18:58 +0900

    Feat : 도메인추가

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,13 @@ export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
       server: {
-        port: 3000,
+        port: 9800,
         host: '0.0.0.0',
+        allowedHosts: [
+      'deegan-propertied-unaudaciously.ngrok-free.dev',
+      // 만약 다른 ngrok 주소를 또 사용할 일이 있다면 여기에 추가
+      '*.ngrok-free.dev' // 또는 와일드카드 문자로 모든 ngrok free 도메인 허용
+        ]
       },
       plugins: [react()],
       define: {
